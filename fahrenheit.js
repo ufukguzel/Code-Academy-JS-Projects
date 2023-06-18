00c3d9dae9c2e95a8bc0ad1142a87fa296dfe78c
Ufuk Güzel <83640138+ufukguzel@users.noreply.github.com>
2023-06-18 20:26:37 +0300

    Kelvin-JS
    This program converts kelvin to celsius and fahrenheit

---
+++ fahrenheit.js
@@ -0,0 +1,10 @@
+const kelvin = 0;
+//I will equated kelvin to 293
+var celsius = kelvin - 273;
+//I found Celsius by subtracting 273 from Kelvin.
+var fahrenheit = celsius * (9/5) + 32;
+//I wrote the formula that calculates fahrenheit
+fahrenheit = Math.floor(fahrenheit);
+//I rounded fahrenheit down
+console.log('The temperature is TEMPERATURE degress Fahrenheit', fahrenheit);
+//ı didnt use the function this challange.
