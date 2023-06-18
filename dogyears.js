88c435059376f79471c371acb19d488ca059713a
Ufuk Güzel <83640138+ufukguzel@users.noreply.github.com>
2023-06-19 00:30:48 +0300

    Dogyears.js
    The program will be calculate the dog year

---
+++ dogyears.js
@@ -0,0 +1,18 @@
+var myAge = 21;
+//I define my age
+var earlyYears = 2;
+//I define dog early years
+earlyYears *=10.5;
+var laterYears = myAge -= 2;
+laterYears *=4;
+// I define laterYears
+var myAgeInDogYears= earlyYears + laterYears;
+//ı collected two variables
+console.log(earlyYears);
+console.log(laterYears);
+var myName = 'Ufuk';
+myName.toLowerCase();
+
+console.log(`My name is ${myName}. I am ${myAge} years old in human years which is ${myAgeInDogYears} years old in dog years.`);
+
+//I finished
