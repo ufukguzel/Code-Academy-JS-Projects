e0c158d297c9d57436c1b83b6243bd95b9fc484a
Ufuk Güzel <83640138+ufukguzel@users.noreply.github.com>
2023-07-05 21:28:25 +0300

    FındIndex.js
    I used fındIndex method

---
+++ findIndex.js
@@ -0,0 +1,15 @@
+const animals = ['hippo', 'tiger', 'lion', 'seal', 'cheetah', 'monkey', 'salamander', 'elephant'];
+
+const foundAnimal = animals.findIndex(animal => {
+  return animal === 'elephant';
+})
+
+console.log(foundAnimal);
+
+const startsWithS = animals.findIndex(animal => {
+  return animal[0] === 's' ? true :false;
+})
+
+console.log(startsWithS);
+
+//I used findIndex method
