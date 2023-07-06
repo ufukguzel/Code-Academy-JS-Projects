bd7c55acefbb5d62f8677e866819378dd1db67d5
Ufuk Güzel <83640138+ufukguzel@users.noreply.github.com>
2023-07-06 12:01:51 +0300

    BelBoy.js
    ı used creat object method

---
+++ belboy.js
@@ -0,0 +1,8 @@
+function BellBoy(name,age,hasWorkPermit,languages){
+    this.name = name;
+    this.age = age;
+    this.hasWorkPermit = hasWorkPermit;
+    this.languages = languages;
+}
+
+var bellBoy1 = new BellBoy("Ufuk",21,true,["Turkısh","English"]);
