be548585927ce034f017ee189f38f1af07da018e
Ufuk Güzel <83640138+ufukguzel@users.noreply.github.com>
2023-07-10 16:33:07 +0300

    spaceShip.js

---
+++ spaceship.js
@@ -0,0 +1,12 @@
+let spaceship = {
+    homePlanet: 'Earth',
+    color: 'silver',
+    'Fuel Type': 'Turbo Fuel',
+    numCrew: 5,
+    flightPath: ['Venus', 'Mars', 'Saturn']
+  };
+  
+  
+  let crewCount = spaceship.numCrew;
+  
+  let planetArray = spaceship.flightPath;
