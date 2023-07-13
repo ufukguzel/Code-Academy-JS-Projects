f516dde4e89d33a6ce309ffcbc77fd2262f82a24
Ufuk Güzel <83640138+ufukguzel@users.noreply.github.com>
2023-07-14 00:01:27 +0300

    hair_color_changed

---
+++ asyenur.js
@@ -0,0 +1,15 @@
+let asyenur = {
+    'hair color' : 'red',
+    homePlanet : 'Earth',
+    color: 'silver',
+    'Secret Mission' : 'Discover life outside of Earth.'
+  };
+  
+  
+  
+
+  aysenur['color'] = 'glorious gold';
+  aysenur.numEngines = 9;
+  
+  delete aysenur['Secret Mission'];
+  
